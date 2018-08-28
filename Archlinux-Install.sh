c302e46d239be21e83694fbccc207015efa5fca3
Marcos Ferreira <alfabech@outlook.com>
2018-08-28 14:53:48 -0300

    temporario

--- Archlinux-Install.sh
+++ Archlinux-Install.sh
@@ -3,6 +3,7 @@ version=1.0
 usuario=marcos
 senha=12345
 host=kleper1
+temporario=`date +%d%H%M`
 
 function wifi(){
 cat << EOF > /etc/netctl/wlp3s1-Edivan2
@@ -136,7 +137,7 @@ elif [ $1 = install ]; then
   wget \
   archlinux-keyring --noconfirm
 
-  useradd temporario -m ; echo temporario:$senha | chpasswd
+  useradd $temporario -m ; echo $temporario:$senha | chpasswd
   echo root:$senha | chpasswd
 
   gpasswd -a $usuario sys
@@ -148,39 +149,39 @@ elif [ $1 = install ]; then
   gpasswd -a $usuario scanner
   gpasswd -a $usuario power
   gpasswd -a $usuario wheel
-  gpasswd -a temporario wheel
+  gpasswd -a $temporario wheel
 
   #usermod -aG libvirt $usuario
   #sed  -i s/\#\ wheel/wheel/g /etc/sudoers ;sed  -i s/\#\ %wheel/%wheel/g /etc/sudoers
   wget -O  /etc/sudoers https://raw.githubusercontent.com/ferreirarocha/arch-installer/master/sudoers
-  rm /home/temporario/.config/yay/config.json
+  rm /home/$temporario/.config/yay/config.json
   mkdir -m 777 pkg
   cd /pkg
-  sudo -u temporario -H sh -c "git clone https://aur.archlinux.org/yay.git; cd yay/ ; makepkg -si --noconfirm"
+  sudo -u $temporario -H sh -c "git clone https://aur.archlinux.org/yay.git; cd yay/ ; makepkg -si --noconfirm"
 
-  su -c " yay -S file-roller                --noconfirm" temporario
-  su -c " yay -S typora                     --noconfirm" temporario
-  su -c " yay -S tilix                      --noconfirm" temporario
-  su -c " yay -S inkscape                   --noconfirm" temporario
-  su -c " yay -S gimp                       --noconfirm" temporario
-  su -c " yay -S atom                       --noconfirm" temporario
-  su -c " yay -S mtnm                       --noconfirm" temporario
-  su -c " yay -S albert                     --noconfirm" temporario
-  su -c " yay -S libreoffice-dev-bin        --noconfirm" temporario
-  su -c " yay -S ksuperkey                  --noconfirm" temporario
-  su -c " yay -S hunspell-pt-br             --noconfirm" temporario
-  su -c " yay -S xfce4-dockbarx-plugin-git  --noconfirm" temporario
-  su -c " yay -S bind-tools                 --noconfirm" temporario
-  su -c " yay -S exfat-utils                --noconfirm" temporario
-  su -c " yay -S xdg-user-dir               --noconfirm" temporario
-  su -c " yay -S telegram-desktop-bin       --noconfirm" temporario
+  su -c " yay -S file-roller                --noconfirm" $temporario
+  su -c " yay -S typora                     --noconfirm" $temporario
+  su -c " yay -S tilix                      --noconfirm" $temporario
+  su -c " yay -S inkscape                   --noconfirm" $temporario
+  su -c " yay -S gimp                       --noconfirm" $temporario
+  su -c " yay -S atom                       --noconfirm" $temporario
+  su -c " yay -S mtnm                       --noconfirm" $temporario
+  su -c " yay -S albert                     --noconfirm" $temporario
+  su -c " yay -S libreoffice-dev-bin        --noconfirm" $temporario
+  su -c " yay -S ksuperkey                  --noconfirm" $temporario
+  su -c " yay -S hunspell-pt-br             --noconfirm" $temporario
+  su -c " yay -S xfce4-dockbarx-plugin-git  --noconfirm" $temporario
+  su -c " yay -S bind-tools                 --noconfirm" $temporario
+  su -c " yay -S exfat-utils                --noconfirm" $temporario
+  su -c " yay -S xdg-user-dir               --noconfirm" $temporario
+  su -c " yay -S telegram-desktop-bin       --noconfirm" $temporario
 
   pacman -R virtualbox-host-dkms \
   virtualbox-sdk \
   virtualbox  --noconfirm
 
   wget -c http://download2267.mediafire.com/z72mlhl7essg/qcbzocqq1c22nbc/conf.zip
-  
+
   unzip -o /pkg/conf.zip -d /home/marcos/
 
   sudo sed -i /etc/lxdm/lxdm.conf \
@@ -189,7 +190,7 @@ elif [ $1 = install ]; then
   useradd $usuario -m ; echo $usuario:$senha | chpasswd
   xdg-user-dirs-update
   chsh -s /bin/zsh $usuario
-  userdel temporario
+  userdel $temporario
   systemctl enable sshd
   sudo systemctl enable lxdm
 
